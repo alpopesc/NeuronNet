dacf11a675151136c2b668881379bc8ad9ba7bfe
Alexander Popescu <alexander.popescu@epfl.ch>
2019-10-21 15:12:57 +0200

    First commit

---
+++ build/CMakeFiles/testNeuronNet.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alpopesc/myfiles/Programmation/Ba3/Small_Project/NeuronNet/src/random.cpp" "/home/alpopesc/myfiles/Programmation/Ba3/Small_Project/NeuronNet/build/CMakeFiles/testNeuronNet.dir/src/random.cpp.o"
+  "/home/alpopesc/myfiles/Programmation/Ba3/Small_Project/NeuronNet/src/test_main.cpp" "/home/alpopesc/myfiles/Programmation/Ba3/Small_Project/NeuronNet/build/CMakeFiles/testNeuronNet.dir/src/test_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/random.cpp
+++ src/random.cpp
@@ -1,3 +1,59 @@
 #include "random.h"
 
+RandomNumbers::RandomNumbers(unsigned long int s) 
+{
+	if (seed == 0) {
+		std::random_device rd;
+		seed = rd();
+	}else{
+		seed(s);
+	}
+	rng = std::mt19937(seed);
+}
+// I took the solution from class
+
+
+void RandomNumbers::uniform_double(std::vector<double>& vec, double lower, double upper);
+{
+	for(size_t i(0); i < vec.size(); i++){
+		vec[i] = uniform_double(lower, upper);
+	}
+}
+
+double RandomNumbers::uniform_double(double lower=0, double upper=1)
+{
+	std::uniform_real_distribution<double> distribution(lower,upper);	
+    return distribution(rng);
+}
+
+void RandomNumbers::normal(std::vector<double>& vec, double mean=0, double sd=1)
+{
+	for(size_t i(0); i < vec.size(); i++){
+		vec[i] = normal(mean, sd);
+	}
+}
+
+double RandomNumbers::normal(double mean=0, double sd=1)
+{
+	if(std_dev < 0){
+		throw string("std_dev can take only positive values");
+	}
+	std::normal_distribution<double> distribution(mean,sd);
+	return distribution(rng);
+}
+
+void RandomNumbers::poisson(std::vector<int>& vec, double mean=1)
+{
+	for(size_t i(0); i < vec.size(); i++){
+		vec[i] = poisson(mean);
+	}
+}
+
+int RandomNumbers::poisson(double mean=1)
+{
+	std::poisson_distribution<double> distribution(mean);
+	return distribution(rng);
+}
+
+
 
