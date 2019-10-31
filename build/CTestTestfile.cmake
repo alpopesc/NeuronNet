e5e0f53cb632d7db4faed69109e6b58b2c16ab57
Alexander Popescu <alexander.popescu@epfl.ch>
2019-10-31 22:22:39 +0100

    Programm does not crash any more and should be working

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/alpopesc/myfiles/Programmation/Ba3/Small_Project/NeuronNet
+# Build directory: /home/alpopesc/myfiles/Programmation/Ba3/Small_Project/NeuronNet/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(NeuronNet "testNeuronNet")
